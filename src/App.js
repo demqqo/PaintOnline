cf7a72be7862e594c37dd7976bb2706aab0e4701
demqqo <138254074+demqqo@users.noreply.github.com>
2024-05-13 18:17:34 -0400

    add components toolbar settingbar canvas, included all of them in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './styles/App/App.css'
+import React from 'react'
+import './styles/app.scss'
+import SettingBar from './components/SettingBar';
+import Toolbar from './components/Toolbar'
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
-      
+        <Toolbar />
+        <SettingBar />
+        <Canvas />
     </div>
   );
 }
---
+++ src/components/SettingBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/toolbar.scss'
+
+const SettingBar = () => {
+  return (
+    <div className="settingbar">
+      
+    </div>
+  )
+}
+
+export default SettingBar
